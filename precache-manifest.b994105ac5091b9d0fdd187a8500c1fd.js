7679e73382098f943c152ba436bc6ed02432215c
Mehmet Kaplan <mehmet.kaplan@gmail.com>
2020-03-29 17:06:06 +0630

    Updates

---
+++ precache-manifest.b994105ac5091b9d0fdd187a8500c1fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f2b0a3531647713c0a68501ead4ad8",
+    "url": "/matrix-card/index.html"
+  },
+  {
+    "revision": "ed4fddf7b7765508ca12",
+    "url": "/matrix-card/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "18ad3a01e145f1e2d683",
+    "url": "/matrix-card/static/js/2.3bba2541.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/matrix-card/static/js/2.3bba2541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed4fddf7b7765508ca12",
+    "url": "/matrix-card/static/js/main.262202c1.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/matrix-card/static/js/main.262202c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da636d1fd0bcc9169c4",
+    "url": "/matrix-card/static/js/runtime-main.7040b499.js"
+  }
+]);
